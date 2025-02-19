af71c3dfc9a3757289c8c09d7fb0550be413e616
Nannasarah <nannasp01@gmail.com>
2025-02-19 10:45:46 +0100

    opskriftlayout

--- opskrift.js
+++ opskrift.js
@@ -16,35 +16,36 @@ fetch(`https://dummyjson.com/recipes/${recipeId}`)
             <div class="recepe_image">
                 <img src="https://cdn.dummyjson.com/recipe-images/${recipeId}.webp" alt="recipe-image">
             </div>
-            <h1>${data.name}</h1>
-            <h2>${data.cuisine}</h2>
+          
             <div class="product_container">
                 <div>
+                  <h1>${data.name}</h1>
+            <h2>${data.cuisine}</h2>
                     <p>${data.instructions}</p>
                 </div>
                 <div class="ingredients_container">
                     <h2>Ingredients</h2>
                     <ul>
-                        <li class="${!data.ingredients[1] && "hide"}">${data.ingredients[1]}</li>
-                        <li class="${!data.ingredients[2] && "hide"}">${data.ingredients[2]}</li>
-                        <li class="${!data.ingredients[3] && "hide"}">${data.ingredients[3]}</li>
-                        <li class="${!data.ingredients[4] && "hide"}">${data.ingredients[4]}</li>
-                        <li class="${!data.ingredients[5] && "hide"}">${data.ingredients[5]}</li>
-                        <li class="${!data.ingredients[6] && "hide"}">${data.ingredients[6]}</li>
-                        <li class="${!data.ingredients[7] && "hide"}">${data.ingredients[7]}</li>
-                        <li class="${!data.ingredients[8] && "hide"}">${data.ingredients[8]}</li>
-                        <li class="${!data.ingredients[9] && "hide"}">${data.ingredients[9]}</li>
-                        <li class="${!data.ingredients[10] && "hide"}">${data.ingredients[10]}</li>
-                        <li class="${!data.ingredients[11] && "hide"}">${data.ingredients[11]}</li>
-                        <li class="${!data.ingredients[12] && "hide"}">${data.ingredients[12]}</li>
-                        <li class="${!data.ingredients[13] && "hide"}">${data.ingredients[13]}</li>
-                        <li class="${!data.ingredients[14] && "hide"}">${data.ingredients[14]}</li>
-                        <li class="${!data.ingredients[15] && "hide"}">${data.ingredients[15]}</li>
-                        <li class="${!data.ingredients[16] && "hide"}">${data.ingredients[16]}</li>
-                        <li class="${!data.ingredients[17] && "hide"}">${data.ingredients[17]}</li>
-                        <li class="${!data.ingredients[18] && "hide"}">${data.ingredients[18]}</li>
-                        <li class="${!data.ingredients[19] && "hide"}">${data.ingredients[19]}</li>
-                        <li class="${!data.ingredients[20] && "hide"}">${data.ingredients[20]}</li>
+                        <li class="${!data.ingredients[1] && "hide"}"><input type="checkbox"> &nbsp ${data.ingredients[1]}</li>
+                        <li class="${!data.ingredients[2] && "hide"}"> <input type="checkbox">&nbsp ${data.ingredients[2]}</li>
+                        <li class="${!data.ingredients[3] && "hide"}"> <input type="checkbox"> &nbsp${data.ingredients[3]}</li>
+                        <li class="${!data.ingredients[4] && "hide"}"> <input type="checkbox"> &nbsp${data.ingredients[4]}</li>
+                        <li class="${!data.ingredients[5] && "hide"}"> <input type="checkbox"> &nbsp${data.ingredients[5]}</li>
+                        <li class="${!data.ingredients[6] && "hide"}"> <input type="checkbox"> &nbsp${data.ingredients[6]}</li>
+                        <li class="${!data.ingredients[7] && "hide"}"> <input type="checkbox">&nbsp ${data.ingredients[7]}</li>
+                        <li class="${!data.ingredients[8] && "hide"}"> <input type="checkbox"> &nbsp${data.ingredients[8]}</li>
+                        <li class="${!data.ingredients[9] && "hide"}"> <input type="checkbox"> &nbsp${data.ingredients[9]}</li>
+                        <li class="${!data.ingredients[10] && "hide"}"> <input type="checkbox">&nbsp ${data.ingredients[10]}</li>
+                        <li class="${!data.ingredients[11] && "hide"}"> <input type="checkbox"> &nbsp${data.ingredients[11]}</li>
+                        <li class="${!data.ingredients[12] && "hide"}"> <input type="checkbox"> &nbsp${data.ingredients[12]}</li>
+                        <li class="${!data.ingredients[13] && "hide"}"> <input type="checkbox"> &nbsp${data.ingredients[13]}</li>
+                        <li class="${!data.ingredients[14] && "hide"}"> <input type="checkbox"> &nbsp${data.ingredients[14]}</li>
+                        <li class="${!data.ingredients[15] && "hide"}"> <input type="checkbox">&nbsp ${data.ingredients[15]}</li>
+                        <li class="${!data.ingredients[16] && "hide"}"> <input type="checkbox"> &nbsp${data.ingredients[16]}</li>
+                        <li class="${!data.ingredients[17] && "hide"}"> <input type="checkbox"> &nbsp${data.ingredients[17]}</li>
+                        <li class="${!data.ingredients[18] && "hide"}"> <input type="checkbox"> &nbsp${data.ingredients[18]}</li>
+                        <li class="${!data.ingredients[19] && "hide"}"> <input type="checkbox"> &nbsp${data.ingredients[19]}</li>
+                        <li class="${!data.ingredients[20] && "hide"}"> <input type="checkbox">&nbsp ${data.ingredients[20]}</li>
                     </ul>
                 </div>
             </div>
